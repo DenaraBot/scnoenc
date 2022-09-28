2d1ada93a3224f14c2a5577853875779e110dfd4
DenaraBot <devanandaarafli@gmail.com>
2022-09-28 09:21:11 +0700

    noenc

---
+++ plugins/donasi.js
@@ -0,0 +1,20 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `
+┌─「 Donasi • Pulsa 」
+│ • *Telkomsel:* [${global.ppulsa}]
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+│ • *Dana:* [${global.pdana}]
+│ • *Gopay:* [${global.pgopay}]
+│ • *Ovo:* [${global.povo}]
+│ • *Link Aja:* [${global.plinkaja}]
+❏────
+`.trim()
+  m.reply(text)
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
